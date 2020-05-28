615730e5e3d1c2f16325e79885dd7cbb4f18e267
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-05-28 14:27:59 -0500

    [INIT]

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Documents Security',
+    'version': '0.1.0',
+    'author': 'HomebrewSoft',
+    'website': 'https://gitlab.com/HomebrewSoft/etm/documents_security',
+    'depends': [
+        'documents',
+    ],
+    'data': [
+        # security
+        'security/documents_security_groups.xml',
+        # data
+        # reports
+        # views
+        'views/assets.xml',
+    ],
+}
